436530d082d09efa21bee0bfe1b3db86fe06506c
Robert Young <robert.young@shopify.com>
2021-08-23 01:20:31 -0400

    Add more things

---
+++ test/bf/test_interpreter.rb
@@ -0,0 +1,27 @@
+# typed: strict
+# frozen_string_literal: true
+
+# require("bf")
+# require("minitest/autorun")
+
+# HELLO_WORLD =
+#   "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
+
+# class TestInterpreter < Minitest::Test
+#   extend T::Sig
+
+#   sig { void }
+#   def test_hello_world
+#     assert_equal("Hello World!\n", BF::Interpreter.call(HELLO_WORLD))
+#   end
+# end
+
+# class TestCompiler < Minitest::Test
+#   extend T::Sig
+
+#   sig { void }
+#   def test_hello_world
+#     # builder = NPC::Boot::Builder.new
+#     # compiler = Compiler.new(builder)
+#   end
+# end
