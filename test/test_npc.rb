97de2246ac27aa4147d2ba3ac3b3a88d227ce58b
Robert Young <robert.young@shopify.com>
2022-02-08 12:33:58 -0500

    Comment out some tests for the "operation definition" DSL

--- test/test_npc.rb
+++ test/test_npc.rb
@@ -6,70 +6,67 @@ require "npc/test"
 class TestNPC < Minitest::Test
   extend T::Sig
 
-  class Const < NPC::Operation
-    define do
-    end
+  # class Const < NPC::Operation
+  #   define do
+  #   end
 
-    sig { params(value: Integer).void }
-    def initialize(value)
-      super([], [])
-      @value = T.let(value, Integer)
-    end
+  #   sig { params(value: Integer).void }
+  #   def initialize(value)
+  #     super(results: [NPC::Core::I32])
+  #     @value = T.let(value, Integer)
+  #   end
 
-    sig { returns(Integer) }
-    attr_accessor :value
-  end
+  #   sig { returns(Integer) }
+  #   attr_accessor :value
+  # end
 
-  class Add < NPC::Operation
-    define do
-      operand(:lhs)
-      operand(:rhs)
-    end
-  end
+  # class Add < NPC::Operation
+  #   define do
+  #     operand(:lhs)
+  #     operand(:rhs)
+  #   end
+  # end
 
-  sig { void }
-  def test_accessors
-    k = Const.new(123)
-    add = Add.new
-    add.lhs = k.result
-    add.rhs = k.result
+  # sig { void }
+  # def test_accessors
+  #   k = Const.new(123)
+  #   add = Add.new
+  #   add.lhs = k.result(0)
+  #   add.rhs = k.result(0)
 
-    assert_equal(add.lhs, add.rhs)
-  end
+  #   assert_equal(add.lhs, add.rhs)
+  # end
 
-  sig { void }
-  def test_main
-    region = NPC::Region.new
+  # sig { void }
+  # def test_main
+  #   region = NPC::Region.new
 
-    assert_empty(region)
-    assert_equal(region.front, region.back)
-    assert_nil(region.first_block)
-    assert_nil(region.last_block)
-    assert_empty(region)
-    assert_equal(region.front, region.back)
-    assert_nil(region.first_block)
-    assert_nil(region.last_block)
+  #   assert_empty(region)
+  #   assert_equal(region.front, region.back)
+  #   assert_nil(region.first_block)
+  #   assert_nil(region.last_block)
+  #   assert_empty(region)
+  #   assert_equal(region.front, region.back)
+  #   assert_nil(region.first_block)
+  #   assert_nil(region.last_block)
 
-    assert_empty(region.arguments)
-  end
+  #   assert_empty(region.arguments)
+  # end
 
-  sig { void }
-  def test_operation
-    block = NPC::Block.new
+  # sig { void }
+  # def test_operation
+  #   block = NPC::Block.new
 
-    assert_empty(block)
-    assert_equal(block.front, block.back)
+  #   assert_empty(block)
+  #   assert_equal(block.front, block.back)
 
-    operation = NPC::Core::I32Const.new(
-      NPC::UnknownLocation.new,
-      1234,
-    )
+  #   operation = NPC::Core::I32Const.new(1234)
 
-    block.append_operation!(operation)
+  #   block.append_operation!(operation)
 
-    assert_equal(operation, block.front.next_operation)
-    assert_equal(operation, block.back)
-  end
+  #   assert_equal(operation, block.front.next_operation)
+  #   assert_equal(operation, block.back)
+  # end
 
   # sig { void }
   # def test_fold
